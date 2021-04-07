f814e820ec9037d5dcada8f3b90e57a124b4bea4
Michał Gleba <GlebaM@users.noreply.github.com>
2021-04-07 17:39:53 +0200

    Setup-colors-fonts

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'title':['Sansita', 'sans-serif'],
+        'body':['Lato', 'sans-serif']
+      },
+      colors:{
+      'pri':"#0083FF",
+      'sec':"#081F4D",
+      "our-bg":"#D9E6FF",
+      "gen":"#3B4D71"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
